9b0b3fde4a2e17af18080fca729601572c8d141a
Viditk <viditkhanaa@gmail.com>
2021-05-22 20:43:26 +0530

    Add playground screen as home

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:thats_my_ball/ui/playground_screen.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: PlaygroundScreen(),
+    );
   }
 }
