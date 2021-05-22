d0b57f8d1d967c0c62f1ef2678699fca86a526cc
Viditk <viditkhanaa@gmail.com>
2021-05-22 23:17:12 +0530

    Refractor code

--- lib/ui/playground_screen.dart
+++ lib/ui/playground_screen.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:thats_my_ball/controllers/ball_position_controller.dart';
 import 'package:thats_my_ball/controllers/game_points_controller.dart';
 import 'package:thats_my_ball/controllers/game_state_controller.dart';
+import 'package:thats_my_ball/ui/widgets/welcome_card.dart';
 import 'widgets/game_timer.dart';
 
 class PlaygroundScreen extends StatefulWidget {
@@ -19,47 +20,12 @@ class _PlaygroundScreenState extends State<PlaygroundScreen> {
 
   Future showWelcomeDialog() async {
     await Future(() {});
-    final GameStateController gameStateCtrl = Get.find();
-    final textTheme = Theme.of(context).textTheme;
     showDialog(
         context: context,
         barrierDismissible: false,
         builder: (context) {
           return Center(
-            child: Card(
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    SizedBox(height: 20),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Text(
-                        "Welcome to 'THAT'S MY BALL'",
-                        style: textTheme.headline4,
-                      ),
-                    ),
-                    SizedBox(height: 32),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Text(
-                        'Click on the ball as many time as you can to get the maximum score',
-                        style: textTheme.headline6,
-                      ),
-                    ),
-                    SizedBox(height: 24),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                        gameStateCtrl.startGame();
-                      },
-                      child: Text('CONTINUE'),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            child: WelcomeCard(),
           );
         });
   }
---
+++ lib/ui/widgets/welcome_card.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:thats_my_ball/controllers/game_state_controller.dart';
+
+class WelcomeCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final GameStateController gameStateCtrl = Get.find();
+    final textTheme = Theme.of(context).textTheme;
+    return Card(
+      margin: EdgeInsets.all(12),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text(
+                  "Welcome to 'THAT'S MY BALL'",
+                  textAlign: TextAlign.center,
+                  style: textTheme.headline4,
+                ),
+              ),
+              SizedBox(height: 32),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text(
+                  'Click on the ball as many time as you can to get the maximum score',
+                  style: textTheme.headline6,
+                ),
+              ),
+              SizedBox(height: 24),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  gameStateCtrl.startGame();
+                },
+                child: Text('CONTINUE'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
